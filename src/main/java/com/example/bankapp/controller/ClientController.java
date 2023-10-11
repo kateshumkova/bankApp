4d3c477e89508276bbffac2d4e843514dd779aff
Maksimov Vladimir <vladimir.maksimov@domrf.ru>
2023-10-11 13:55:11 +0300

    fix-mapper

--- src/main/java/com/example/bankapp/controller/ClientController.java
+++ src/main/java/com/example/bankapp/controller/ClientController.java
@@ -3,16 +3,15 @@ package com.example.bankapp.controller;
 import com.example.bankapp.entities.ClientEntity;
 import com.example.bankapp.service.ClientService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/client")
+@RequiredArgsConstructor
 public class ClientController {
     private final ClientService clientService;
 
-    public ClientController(ClientService clientService) {
-        this.clientService = clientService;
-    }
 
     public List<ClientEntity> getAll(){
         return clientService.getAll();
--- src/main/java/com/example/bankapp/service/impl/ClientServiceImpl.java
+++ src/main/java/com/example/bankapp/service/impl/ClientServiceImpl.java
@@ -3,18 +3,15 @@ package com.example.bankapp.service.impl;
 import com.example.bankapp.entities.ClientEntity;
 import com.example.bankapp.repository.ClientRepository;
 import com.example.bankapp.service.ClientService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
 
     private final ClientRepository clientRepository;
-
-    public ClientServiceImpl(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
 
 
     @Override
